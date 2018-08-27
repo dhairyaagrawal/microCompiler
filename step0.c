b64dde2b34764455e4abea0eff08fcb8272d9f7a
Dhairya Agrawal <dhairyaagrawal@gmail.com>
2018-08-27 17:58:21 -0400

    initial .c file for step 0

---
+++ step0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("Team: Dhairya_Dennis\n\n");
+	printf("Dhairya Agrawal\n");
+	printf("dhairyaagrawal\n\n");
+	printf("Dennis Trzaska\n");
+	printf("dtrzaska\n");
+
+	return 0;
+}
